5cd8580ad02d1f079decc6ec96b08aaff7bfbc4f
shuijing117 <1004319075@qq.com>
2015-09-29 23:18:38 +0800

    add DoubleLinkList to src

--- DoubleLinkList/DoubleLinkList.h
+++ DoubleLinkList/DoubleLinkList.h
@@ -17,4 +17,119 @@ typedef struct _DLINKLIST {
 	struct _DLINKLIST *next;
 }*DoubleLinkList, *Position, Node;
 
+int InitDoubleLinkList(DoubleLinkList *L);
+int DestoryDoubleLinkList(DoubleLinkList *L);
+int HeadInsert(DoubleLinkList *L, datatype data);
+int TailInsert(DoubleLinkList *L, datatype data);
+int ClearDoubleLinkList(DoubleLinkList *L);
+
+int ClearDoubleLinkList(DoubleLinkList *L)
+{
+	Node *p, *q;
+
+	if(L == NULL || *L == NULL) {
+		return -1;
+	}
+
+	p = *L;
+	while(p->next) {
+		p = p->next;
+	}
+	q = p->prior;
+	while(p != *L) {
+		free(p);
+		p = q;
+		q = q->prior;
+	}
+	(*L)->next = NULL;
+
+
+	return 0;
+}
+
+int TailInsert(DoubleLinkList *L, datatype data)
+{
+	Node *p, *q;
+
+	if(L == NULL || *L == NULL) {
+		return -1;
+	}
+
+	p = (Node *)malloc(sizeof(Node));
+	if(p == NULL) {
+		return -2;
+	}
+	p->data = data;
+
+	q = *L;
+	while(q->next) {
+		q = q->next;
+	}
+	p->next = q->next;
+	p->prior = q;
+	q->next = p;
+
+	return 0;
+}
+
+int HeadInsert(DoubleLinkList *L, datatype data)
+{
+	Node *p;
+
+	if(L == NULL || *L == NULL) {
+		return -1;
+	}
+
+	p = (Node *)malloc(sizeof(Node));
+	if(p == NULL) {
+		return -2;
+	}
+	p->data = data;
+	p->prior = *L;
+	p->next = (*L)->next;
+
+	if((*L)->next != NULL)
+		(*L)->next->prior = p;
+
+	(*L)->next = p;
+
+	return 0;
+}
+
+int DestoryDoubleLinkList(DoubleLinkList *L)
+{
+	DoubleLinkList p, q;
+	if(L == NULL || *L == NULL) {
+		return -1;
+	}
+
+	q = *L;
+	p = q->next;
+	while(p != NULL) {
+		free(q);
+		q = p;
+		p = p->next;
+	}
+	free(q);
+
+	return 0;
+}
+
+int InitDoubleLinkList(DoubleLinkList *L)
+{
+	if(L == NULL) {
+		return -1;
+	}
+
+	*L = (DoubleLinkList)malloc(sizeof(Node));
+	if(*L == NULL) {
+		return -1;
+	}
+	(*L)->prior = NULL;
+	(*L)->next = NULL;
+
+	return 0;
+}
+
+
 #endif
--- DoubleLinkList/test.c
+++ DoubleLinkList/test.c
@@ -7,10 +7,51 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "DoubleLinkList.h"
 
+void Print(DoubleLinkList L);
+
+void Print(DoubleLinkList L)
+{
+	int i;
+	DoubleLinkList p = L;
+
+	for(i = 1; p != NULL; i++, p = p->next) {
+		printf("%d-%p:%d ", i, p, p->data);
+	}
+	printf("\n");
+}
+
 int main(void)
 {
+	int i;
+	DoubleLinkList L;
+
+	InitDoubleLinkList(&L);
+	printf("Init:\n");
+	Print(L);
+
+	srand((int)time(NULL));
+
+	for(i = 0; i< 5; i++) {
+		HeadInsert(&L, rand()%100);
+	}
+	printf("HeadInsert:\n");
+	Print(L);
+
+	for(i = 0; i< 5; i++) {
+		TailInsert(&L, rand()%100);
+	}
+	printf("TailInsert:\n");
+	Print(L);
+
+	ClearDoubleLinkList(&L);
+	Print(L);
+
+	DestoryDoubleLinkList(&L);
+
 	return 0;
 }
