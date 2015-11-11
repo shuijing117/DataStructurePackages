420a5c1733d7526caa2fe03a9f2cd75e14339091
shuijing117 <1004319075@qq.com>
2015-11-11 20:19:35 +0800

    Add Stack to Src

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -9,6 +9,10 @@
 
 #include <malloc.h>
 
+#define		TRUE	1
+#define		FALSE	0
+
+typedef int BOOL;
 typedef int datatype;
 
 typedef struct _STACK {
@@ -17,101 +21,101 @@ typedef struct _STACK {
 	int size;
 }Stack;
 
-int InitStack(Stack *s, int size);
-int DestoryStack(Stack *s);
-int IsEmpty(Stack s);
-int IsFull(Stack s);
-int Push(Stack *s, datatype data);
-int Pop(Stack *s, datatype *data);
-int ClearStack(Stack *s);
-int GetTop(Stack s, datatype *data);
+BOOL InitStack(Stack *s, int size);
+BOOL DestoryStack(Stack *s);
+BOOL IsEmpty(Stack s);
+BOOL IsFull(Stack s);
+BOOL Push(Stack *s, datatype data);
+BOOL Pop(Stack *s, datatype *data);
+BOOL ClearStack(Stack *s);
+BOOL GetTop(Stack s, datatype *data);
 
-int GetTop(Stack s, datatype *data)
+BOOL GetTop(Stack s, datatype *data)
 {
 	if(IsEmpty(s)) {
-		return -1;
+		return FALSE;
 	}
 	*data = s.data[s.top];
 
-	return 0;
+	return TRUE;
 }
 
-int ClearStack(Stack *s)
+BOOL ClearStack(Stack *s)
 {
 	if(s == NULL) {
-		return -1;
+		return FALSE;
 	}
 
 	s->top = -1;
 
-	return 0;
+	return TRUE;
 }
 
-int Pop(Stack *s, datatype *data)
+BOOL Pop(Stack *s, datatype *data)
 {
 	if(s = NULL) {
-		return -1;
+		return FALSE;
 	}
 
 	if(IsEmpty(*s)) {
-		return -2;
+		return FALSE;
 	}
 
 	*data = s->data[(s->top)--];
 
-	return 0;
+	return TRUE;
 }
 
-int Push(Stack *s, datatype data)
+BOOL Push(Stack *s, datatype data)
 {
 	if(s = NULL) {
-		return -1;
+		return FALSE;
 	}
 
 	if(IsFull(*s)) {
-		return -2;
+		return FALSE;
 	}
 
 	s->data[++(s->top)] = data;
 
-	return 0;
+	return TRUE;
 }
 
-int IsFull(Stack s)
+BOOL IsFull(Stack s)
 {
 	return s.top >= s.size-1;
 }
 	
-int IsEmpty(Stack s)
+BOOL IsEmpty(Stack s)
 {
 	return s.top <= -1;
 }
 
-int DestoryStack(Stack *s)
+BOOL DestoryStack(Stack *s)
 {
 	if(s == NULL) {
-		return -1;
+		return FALSE;
 	}
 
 	free(s->data);
 
-	return 0;
+	return TRUE;
 }
 
-int InitStack(Stack *s, int size)
+BOOL InitStack(Stack *s, int size)
 {
 	if(s == NULL) {
-		return -1;
+		return FALSE;
 	}
 
 	s->data = (datatype *)malloc(sizeof(datatype) * size);
 	if(s->data == NULL) {
-		return -2;
+		return FALSE;
 	}
 	s->size = size;
 	s->top = -1;
 
-	return 0;
+	return TRUE;
 }
 
 #endif
